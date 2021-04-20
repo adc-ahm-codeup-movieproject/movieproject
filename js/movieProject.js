d10723447b4f5efd9e61365708046425faf52986
Nathan Adcock <nathan.adcock43@gmail.com>
2021-04-20 12:03:59 -0500

    feat: Movie Project: fixed headers

--- js/movieProject.js
+++ js/movieProject.js
@@ -3,25 +3,50 @@
 
 const OmdbAPIurl = "http://www.omdbapi.com/";
 
-const ourMovieDB = "https://glitch.com/edit/#!/certain-careful-scent"
+const ourMovieDB = "https://certain-careful-scent.glitch.me/movies"
 
 
 
-let movieTitle = {title: "Fetch Requests", body: "are a fun way to use JS"} // the what (the cover letter: fax metaphor)
+let movieTitle = {
+    title: "Hello",
+    rating: "5",
+    poster: "",
+    year: "2002",
+    genre: "Action, Horror, Sci-Fi",
+    director: "Paul W.S. Anderson",
+    plot: "A special military unit fights a powerful, out-of-control supercomputer and hundreds of scientists who have mutated into flesh-eating creatures after a laboratory accident.",
+    actors: "Ryan McCluskey, Oscar Pearce, Indra Ové, Anna Bolt"} ;
 
-let options = {
+let movieDbOptions = {
     method: "POST",
-    header: {
+    headers: {
         "Content-Type": "application/json",
     },
     body: JSON.stringify(movieTitle)
-} // the necessary identifying options (the : fax metaphor)
+}
 
-   fetch(ourMovieDB, options).then(function(response) {
+   fetch(ourMovieDB, movieDbOptions).then(function(response) {
        console.log(response);
+       console.log(movieDbOptions)
+       console.log(movieTitle)
    });
 
-  let url = "/posts";
+  // let url = "/posts";
 
-  console.log(movieTitle);
-
+// let starWarsAPI = fetch("https://swapi.dev/api/films")
+//     //  Parse the response into json
+//     .then((response)=>{return response.json()})
+//     // access the results property from the json Object
+//     .then((jsonData)=>{
+//         console.log(jsonData);
+//         return jsonData.results;
+//     })
+//     // iterate over the results array, and access title and id properties of each film
+//     .then((results)=>{
+//         results.forEach((film)=>console.log(`${film.title} ${film.episode_id}`))
+//     })
+//     // This runs if my fetch request fails. Try changing films to xfilms in the url and look at the error that gets passed. It isn't what you think it would be.
+//     .catch(error => console.log('Error getting films.', error));
+//
+// // Notice how this log runs before the we log the results.
+// console.log('starWarsAPI Promise', starWarsAPI);
